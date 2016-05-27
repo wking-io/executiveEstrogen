588044b8433bfcd7da188cb29725be77b63ff47d
William King <wkingcreative@gmail.com>
2016-05-27 00:47:07 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+  //hamburger toggle
+  $('.nav__icon').click(function(){
+    $(this).toggleClass('open');
+    $('#menu').toggleClass('menu-open');
+  });
+  //end hamburger toggle
+
+  //menu close on link click
+  $('#mobile-about, #mobile-services, #mobile-testimonials, #mobile-contact').click(function(){
+    $('.nav__icon').toggleClass('open');
+    $('#menu').toggleClass('menu-open');
+  });
+  //end menu close
+
+  //WOW animate
+  var wow = new WOW(
+    {
+      boxClass:     'wow',      // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+      offset:       0,          // distance to the element when triggering the animation (default is 0)
+      mobile:       true,       // trigger animations on mobile devices (default is true)
+      live:         true,       // act on asynchronously loaded content (default is true)
+      callback:     function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+      },
+      scrollContainer: null // optional scroll container selector, otherwise use window
+    }
+  );
+  wow.init();
+});
